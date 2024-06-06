0ab802e4cd2a61082a822a269131fb76c83e5c22
lokeshkarthi25 <150770253+lokeshkarthi25@users.noreply.github.com>
2024-06-06 18:44:25 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,33 @@
+export let data =[
+ {
+    item:{
+        id:"1",
+        name: "Basa Fish",
+        describe:"Pungus/Keluthi Meen/Assam Vala",
+        img:"https://www.licious.in/_next/image?url=https%3A%2F%2Fassets.licious.in%2FOMS-ProductMerchantdising%2F16b289f4-f903-61f7-fadd-f83b2b86e654%2Foriginal%2F1699204642806.jpg&w=384&q=75",
+        weight:"500g|12-18 pieces|serves 4",
+        price:"₹400/-"
+    }
+},
+    {
+    item1 :{ 
+        id:"2", 
+        name: "Chicken",
+        describe:"Juicy bone-in & boneless chicken for delectable curries",
+        img:"https://themegenix.net/html/bemet/assets/img/images/features_img02.png",
+        weight:"500g|12-18 pieces|serves 4",
+        price:"₹350/-"
+    }},
+    {
+    item2 :{
+        id:"3",
+        name: "Chicken Breast - Boneless",
+        describe:"Enjoy tender pieces of juicy chicken breast",
+        img:"https://www.licious.in/_next/image?url=https%3A%2F%2Fassets.licious.in%2FOMS-ProductMerchantdising%2Fd7f6c192-6591-4894-fa08-f4fcaaeaa8d1%2Foriginal%2F1716980541332.png&w=384&q=75",
+        weight:"500g|12-18 pieces|serves 4",
+        price:"₹250/-"
+    }
+    }]
+
+
+
